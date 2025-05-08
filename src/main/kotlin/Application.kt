eb718ef386e7745d9ce1e7222d42cdf6f7c06daa
kun <sokkun.norn2019@gmail.com>
2025-05-08 13:48:29 +0700

    init project

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,12 @@
+package me.learning
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureRouting()
+}
