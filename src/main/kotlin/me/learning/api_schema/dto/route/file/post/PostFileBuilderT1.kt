c946f0845b2658c6bd4f710e3f4e1378f48c1629
kun <sokkun.norn2019@gmail.com>
2025-05-27 13:51:33 +0700

    implement extension function post with file dto

---
+++ src/main/kotlin/me/learning/api_schema/dto/route/file/post/PostFileBuilderT1.kt
@@ -0,0 +1,40 @@
+package me.learning.api_schema.dto.route.file.post
+
+import io.ktor.server.routing.Route
+import me.learning.api_schema.common.RouteFormDataPropEnum
+import kotlin.reflect.KClass
+
+data class PostFileBuilderT1<T1 : Any>(
+    val route: Route,
+    val path: String,
+    val extensions: List<String>,
+    val removeFileAfterProcessing: Boolean = false,
+    val p1: Pair<KClass<T1>, RouteFormDataPropEnum>
+) {
+
+    init {
+        val hasFile = RouteFormDataPropEnum.FILE == p1.second
+        val hasFiles = RouteFormDataPropEnum.FILES == p1.second
+        if (!hasFiles && !hasFile) throw IllegalArgumentException("Unsupported property type not file")
+    }
+
+    fun <T : Any> addProp(pair: Pair<KClass<T>, RouteFormDataPropEnum>, extensions: List<String>): PostFileBuilderT2<T1, T> {
+        val ext = when (true) {
+            this.extensions.isNotEmpty() -> this.extensions
+            else -> extensions
+        }
+
+        return PostFileBuilderT2(
+            route,
+            path,
+            ext,
+            removeFileAfterProcessing,
+            p1,
+            pair
+        )
+    }
+
+    inline fun <reified T : Any> auth() = addProp(Pair(T::class, RouteFormDataPropEnum.AUTH), extensions)
+
+    inline fun <reified T : Any> data() = addProp(Pair(T::class, RouteFormDataPropEnum.TEXT), extensions)
+}
