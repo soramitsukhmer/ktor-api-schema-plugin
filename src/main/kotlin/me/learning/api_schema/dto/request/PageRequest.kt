58f2f39e1200fafb6b40ea3ace83f8f1151d07db
kun <sokkun.norn2019@gmail.com>
2025-08-08 14:56:19 +0700

    refactoring code

---
+++ src/main/kotlin/me/learning/api_schema/dto/request/PageRequest.kt
@@ -0,0 +1,14 @@
+package me.learning.api_schema.dto.request
+
+import me.learning.api_schema.common.DirectionEnum
+
+abstract class PageRequest(
+    open val size: Int = 10,
+    open val page: Long = 0,
+    open val sort: List<Order> = emptyList()
+)
+
+data class Order(
+    val property: String,
+    val direction: DirectionEnum
+)
